06a19dbfdf0e3003fb89db256a102dcc6245bc2e
Mati Reyna <matiireyna@hotmail.com>
2025-12-14 18:02:39 -0300

    Renderizamos el on boarding correctamente

--- client/App.js
+++ client/App.js
@@ -1,19 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StyleSheet, Text, View } from 'react-native';
+
+import OnboardingScreen from './src/screens/OnboardingScreen';
 
 import { COLORS } from './src/constants/colors';
 
 export default function App() {
   return (
-    <View style={ styles.loadingContainer }>
-      <StatusBar style='light' />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar style='light' backgroundColor={ COLORS.background } />
+      <OnboardingScreen />
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    backgroundColor: COLORS.background
-  }
-});
+const styles = StyleSheet.create({});
